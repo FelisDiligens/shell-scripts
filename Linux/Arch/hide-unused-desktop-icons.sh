090a8882cf8e74908bc3c71effa6e273b2b92ecf
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-07-14 21:04:26 +0200

    Add more to hide

--- Linux/Arch/hide-unused-desktop-icons.sh
+++ Linux/Arch/hide-unused-desktop-icons.sh
@@ -14,15 +14,24 @@ function no-display {
 }
 
 path="/usr/share/applications"
-no-display "$path/stoken-gui.desktop"
-no-display "$path/stoken-gui-small.desktop"
-no-display "$path/qv4l2.desktop"
-no-display "$path/qvidcap.desktop"
-no-display "$path/lstopo.desktop"
+no-display "$path/stoken-gui.desktop" # Software Token
+no-display "$path/stoken-gui-small.desktop" # Software Token (small)
+no-display "$path/qv4l2.desktop" # Qt V4L2 test Utility
+no-display "$path/qvidcap.desktop" # Qt V4L2 video capture utility
+no-display "$path/lstopo.desktop" # Hardware Locality lstopo
 no-display "$path/yad-icon-browser.desktop"
 no-display "$path/yad-settings.desktop"
-no-display "$path/avahi-discover.desktop"
-no-display "$path/bssh.desktop"
-no-display "$path/bvnc.desktop"
+no-display "$path/avahi-discover.desktop" # Avahi Zeroconf Browser
+no-display "$path/bssh.desktop" # Avahi SSH Server Browser
+no-display "$path/bvnc.desktop" # Avahi VNC Server Browser
 no-display "$path/xterm.desktop"
+no-display "$path/uxterm.desktop"
 no-display "$path/cups.desktop"
+# no-display "$path/org.gnome.Meld.desktop"
+# no-display "$path/pavucontrol.desktop" # PulseAudio volume control
+# KDE:
+no-display "$path/assistant.desktop" # Qt Assistant
+no-display "$path/designer.desktop" # Qt Designer
+no-display "$path/linguist.desktop" # Qt Linguist
+no-display "$path/qdbusviewer.desktop" # Qt QDBusViewer
+no-display "$path/org.kde.kuserfeedback-console.desktop" # UserFeedback-Konsole
