841bf0b5c342d1025d046a21bb267439a3989bd1
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-07-27 10:19:30 +0200

    Updated

--- Linux/Docker/docker-ports.sh
+++ Linux/Docker/docker-ports.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 docker container ls --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}" -a | \
     sed -E "s/PORTS/HOST PORTS/" | \
-    sed -E "s/0\.0\.0\.0:[0-9]+->/IPv4: /g" | \
-    sed -E "s/127\.0\.0\.1:[0-9]+->/IPv4: /g" | \
-    sed -E "s/:::[0-9]+->/IPv6: /g" | \
-    sed -E "s/::1:[0-9]+->/IPv6: /g"
+    sed -E "s/:([0-9]+)->[0-9]+\//: \1\//g" | \
+    sed -E "s/0\.0\.0\.0: /IPv4: /g" | \
+    sed -E "s/127\.0\.0\.1: /IPv4: /g" | \
+    sed -E "s/::: /IPv6: /g" | \
+    sed -E "s/::1: /IPv6: /g"
